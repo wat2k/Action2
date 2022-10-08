fbb1afbe872406026a776ea6a683c62aecbca829
wat2k <wat2k@hotmail.com>
2022-10-08 23:10:44 +0700

    Create UE game project

---
+++ Source/Action2/Action2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Action2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Action2, "Action2" );
---
+++ Source/Action2/Action2GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Action2GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTION2_API AAction2GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
